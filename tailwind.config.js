ceb09a6f3cff8e426932a20368c25bd5b6d53fce
Gabriel <gabertrol@gmail.com>
2024-10-17 10:01:02 -0300

    criei a config de cores no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary100: "#FF983F",
+        primary200: "#FF6600",
+        primary300: "#FFFFA1",
+      },  
+      textColor: {
+        text100: "#1D1F21",
+        text200: "#F5F5F5",
+        text300: "#C4C5C4",
+      },
+      backgroundColor: {
+        bg100: "#FFFFFF",
+        bg200: "#FAFAFA",
+        bg300: "#CCCCCC",
+      }
+    },
   },
   plugins: [],
 }
