6ca52d2fdad18d8de190ef671208f45cd9dd0844
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-17 10:58:57 -0300

    fix

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/messagens` | `/(tabs)/notifications` | `/(tabs)/profile` | `/(tabs)\announce` | `/(tabs)\menu` | `/_sitemap` | `/explore` | `/messagens` | `/notifications` | `/onboarding` | `/profile` | `/signin` | `/signup` | `/welcome`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/messagens` | `/(tabs)/notifications` | `/(tabs)/profile` | `/(tabs)\_layout` | `/(tabs)\announce` | `/(tabs)\menu` | `/_sitemap` | `/explore` | `/messagens` | `/notifications` | `/onboarding` | `/profile` | `/signin` | `/signup` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
