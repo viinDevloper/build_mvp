dc17feda636de9e6242cfaf32fde774c65273e85
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-17 11:45:38 -0300

    modified:   src/app/(tabs)/messagens.tsx modified:   src/app/signup.tsx	new file:   src/assets/Frame 17.png

--- src/app/(tabs)/messagens.tsx
+++ src/app/(tabs)/messagens.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { Link } from 'expo-router'
 
 const Messagens = () => {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text>Messagens</Text>
+
     </View>
   )
 }
