2b27878e4eb1b7bd48fbe1a8fa7d1affb6b9deee
Samuel <samueld.r.abreu@gmail.com>
2024-10-18 10:10:01 -0300

    Corrige a sombra do component Input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,7 +6,16 @@ export const Input = ({ ...props }: TextInputProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <View className={`bg-bg200 w-full rounded-lg p-3 mb-4 shadow-md ${isFocused ? 'border-2 border-primary200' : 'border-none'}`}>
+    <View 
+      className={`bg-bg200 w-full rounded-lg p-3 mb-4 ${isFocused ? 'border-2 border-primary200' : 'border-none'}`}
+      style={{
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5, // Para Android
+      }}
+    >
       <TextInput
         className="text-text100" 
         {...props} 
