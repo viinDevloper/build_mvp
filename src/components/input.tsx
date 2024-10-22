ff70dbbd2b6b9bc49a00cc782aa4a85db4f5506f
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 16:20:56 -0300

    refactor: Clean up Input component with improved formatting and style consistency
    - Improved layout and readability by organizing code structure
    - Simplified placeholderTextColor prop for better clarity

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import { TextInput, TextInputProps, View } from "react-native"
-
+import { TextInput, TextInputProps, View } from "react-native";
 
 export const Input = ({ ...props }: TextInputProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <View 
-      className={`bg-bg200 w-full rounded-lg p-3 mb-4 ${isFocused ? 'border-2 border-primary200' : 'border-none'}`}
+    <View
+      className={`bg-bg200 w-full rounded-lg p-3 mb-4 ${
+        isFocused ? "border-2 border-primary200" : "border-none"
+      }`}
       style={{
-        shadowColor: '#000',
+        shadowColor: "#000",
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
@@ -17,13 +18,12 @@ export const Input = ({ ...props }: TextInputProps) => {
       }}
     >
       <TextInput
-        className="text-text100" 
-        {...props} 
-        placeholderTextColor={'#C4C5C4'}
+        className="text-text100"
+        {...props}
+        placeholderTextColor="#C4C5C4"
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
-        
     </View>
-  )
-}
+  );
+};
