7b8ab1f5a95ac8c60b11ed873a3bc4f58b6760f9
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 16:19:10 -0300

    refactor: Clean up Section component by improving formatting and removing unused imports
    - Removed unused Pressable import
    - Improved code readability with better formatting

--- src/components/home/section/index.tsx
+++ src/components/home/section/index.tsx
@@ -1,12 +1,14 @@
-import { View, Text, Pressable } from 'react-native'
-interface Props{
-    name: string;
-    size: "text-lg" | "text-xl" | "text-2xl";
+import { View, Text } from "react-native";
+
+interface Props {
+  name: string;
+  size: "text-lg" | "text-xl" | "text-2xl";
 }
-export function Section({name, size}: Props) {
+
+export function Section({ name, size }: Props) {
   return (
-    <View className='w-full flex flex-row items-center justify-between'>
+    <View className="w-full flex flex-row items-center justify-between">
       <Text className={`${size} font-semibold my-4 self-start`}>{name}</Text>
     </View>
-  )
+  );
 }
