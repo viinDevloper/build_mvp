82b5575b9b8a1bd34af51a9ddf7cac856e042f15
juliaevangelista <juliaevangelista70@gmail.com>
2024-10-22 12:04:36 -0300

    opacity

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,45 +1,45 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Link } from "expo-router";
-import { Feather, Ionicons } from "@expo/vector-icons";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Constants from "expo-constants";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 const StatusBarHeight = Constants.statusBarHeight;
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
+  const [isFocused, setIsFocused] = useState(false);
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <SafeAreaView
         className="flex-1 max-w-[80%] items-center flex-row absolute top-0 justify-center"
         style={{ marginTop: StatusBarHeight }}
       >
         <View className="flex-row border border-slate-950 h-18 rounded-lg bg-transparent p-3">
           <TextInput
-            placeholder="Buscar"
-            className="flex-1 h-full bg-transparent"
+            placeholder={isFocused ? "" : "Buscar"}
+            className="flex-1 h-full bg-transparent text-[14px]"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
           />
           <View className="h-18 w-[1.5px] bg-slate-950 rounded-lg mr-[13px]" />
           <Ionicons name="search-sharp" size={30} color="#020617" />
         </View>
-        <View className="mx-3">
-          <Ionicons name="notifications-outline" size={36} color="black" />
-          <View className="rounded-full bg-primary100 items-center w-5 h-5 justify-center absolute top-0 ml-[18px]"><Text className="text-white">1</Text></View>
-        </View>
+        <TouchableOpacity
+          activeOpacity={0.1}
+          onPress={() => console.log("Notificações clicadas!")}
+        >
+          <View className="mx-3">
+            <Ionicons name="notifications-outline" size={36} color="black" />
+            <View className="rounded-full bg-primary100 items-center w-5 h-5 justify-center absolute top-0 ml-[18px]">
+              <Text className="text-white">1</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
       </SafeAreaView>
-
-      
     </View>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
