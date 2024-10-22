b76ac4c166db36070807c905ddb4426f4d940b25
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 16:14:58 -0300

    refactor: Clean up Banner component with dynamic banner list and improved structure
    - Replaced repeated code with a dynamic banner array
    - Updated console log messages to reflect correct banner ID
    - Improved readability and scalability of the component

--- src/components/home/banner/index.tsx
+++ src/components/home/banner/index.tsx
@@ -1,18 +1,24 @@
 import { View, Pressable, Image, ScrollView } from "react-native";
 
 export function Banner() {
+  const banners = [
+    { id: "1", image: require("../../../assets/banner.jpg") },
+    { id: "2", image: require("../../../assets/banner.jpg") },
+    { id: "3", image: require("../../../assets/banner.jpg") },
+  ];
+
   return (
     <View className="flex-1 mt-3">
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <Pressable key="1" onPress={() => console.log("clicou no banner 1")} className="mr-4">
-          <Image source={require("../../../assets/banner.jpg")} className="w-80 h-40 rounded-xl"/>
-        </Pressable>
-        <Pressable key="2" onPress={() => console.log("clicou no banner 1")} className="mr-4">
-          <Image source={require("../../../assets/banner.jpg")} className="w-80 h-40 rounded-xl"/>
-        </Pressable>
-        <Pressable key="3" onPress={() => console.log("clicou no banner 1")} className="mr-4">
-          <Image source={require("../../../assets/banner.jpg")} className="w-80 h-40 rounded-xl"/>
-        </Pressable>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {banners.map((banner) => (
+          <Pressable
+            key={banner.id}
+            onPress={() => console.log(`Clicou no banner ${banner.id}`)}
+            className="mr-4"
+          >
+            <Image source={banner.image} className="w-80 h-40 rounded-xl" />
+          </Pressable>
+        ))}
       </ScrollView>
     </View>
   );
