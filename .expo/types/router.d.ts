a2a989016c33c0c5d81c4b2e4932fe5355a68330
juliaevangelista <juliaevangelista70@gmail.com>
2024-10-22 14:06:09 -0300

    tabs color white

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/..\components\home\localization\` | `/..\components\home\search\` | `/..\components\localization\` | `/..\components\search\` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -9,36 +9,36 @@ export default function TabLayout() {
       tabBarStyle: { 
         backgroundColor: '#FF6600', 
         borderTopColor: '#ccc' }, 
-        tabBarLabelStyle: { fontSize: 14, color: '#FFFFA1'},
+        tabBarLabelStyle: { fontSize: 14, color: '#FFF'},
         }}>
       <Tabs.Screen name='index' options={{
         title: 'Inicio',
         tabBarIcon: ({}) => (
-          <Octicons name='home' size={22} color={'#FFFFA1'} />
+          <Octicons name='home' size={22} color={'#FFF'} />
         )
       }} />
       <Tabs.Screen name='explore' options={{
         title: 'Buscar',
         tabBarIcon: ({}) => (
-          <Ionicons name='search-sharp' size={22} color={'#FFFFA1'} />
+          <Ionicons name='search-sharp' size={22} color={'#FFF'} />
         )
       }} />
       <Tabs.Screen name='announce' options={{
         title: 'Anunciar',
         tabBarIcon: ({}) => (
-          <Ionicons name='add-circle-outline' size={25} color={'#FFFFA1'} />
+          <Ionicons name='add-circle-outline' size={25} color={'#FFF'} />
         )
       }} />
       <Tabs.Screen name='messagens' options={{
         title: 'Mensagens',
         tabBarIcon: ({}) => (
-          <Ionicons name='chatbubbles-sharp' size={22} color={'#FFFFA1'} />
+          <Ionicons name='chatbubbles-sharp' size={22} color={'#FFF'} />
         )
       }} />
       <Tabs.Screen name='menu' options={{
         title: 'Menu',
         tabBarIcon: ({}) => (
-          <Ionicons name='menu-outline' size={27} color={'#FFFFA1'} />
+          <Ionicons name='menu-outline' size={27} color={'#FFF'} />
         )
       }} />
     </Tabs>
