9db45c6578aa16f4cc28b631dbfb474695cb903c
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 10:48:40 -0300

    git commit -m
    Added Welcome screen layout with logo and styled Login/Create Account buttons using Tailwind CSS.

--- src/app/welcome.tsx
+++ src/app/welcome.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 
 const Welcome = () => {
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View className="flex-1 bg-white justify-center items-center ">
+      {/* Logo Section */}
+      <View className="items-center mb-[296px]">
+        <Image
+          source={require("../assets/b2Y.jpg")}
+          style={{ width: 250, height: 45 }}
+        />
+      </View>
+
+      {/* Login Button */}
+      <TouchableOpacity className="bg-orange-500 w-[347px] rounded-md py-4 mb-4">
+        <Text className="text-white text-center text-lg">Login</Text>
+      </TouchableOpacity>
+
+      {/* Criar Conta Button */}
+      <TouchableOpacity className="border border-orange-500 w-[347px] rounded-md py-4">
+        <Text className="text-orange-500 text-center text-lg">Criar conta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
