92c2d1d8abf660bd946e9e7f60331dcab532c6f2
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 14:57:34 -0300

    modified:   .expo/types/router.d.ts

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,11 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
+<<<<<<< Updated upstream
       StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/..\components\core\Notification` | `/..\components\core\ReviewManager` | `/..\components\home\image-picker\` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
+=======
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/(tabs)\` | `/(tabs)\_layout` | `/Localization` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
+>>>>>>> Stashed changes
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
