ec542f1b9fe5046cf877fe0e1b5a6593245e797f
Vinicius Oliveira Moreira <digitaltsx.vinicius@outlook.com>
2024-10-22 14:56:47 -0300

    new file:   src/components/core/Notification.tsx new file: src/components/core/ReviewManager.tsxnew file:   src/components/home/image-picker/index.tsx

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/(tabs)\` | `/(tabs)\_layout` | `/..\components\home\localization\` | `/..\components\home\search\` | `/Localization` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/announce` | `/(tabs)/explore` | `/(tabs)/menu` | `/(tabs)/messagens` | `/..\components\core\Notification` | `/..\components\core\ReviewManager` | `/..\components\home\image-picker\` | `/_sitemap` | `/announce` | `/explore` | `/localization` | `/menu` | `/messagens` | `/notifications` | `/onboarding` | `/signin` | `/signup` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- src/app/(tabs)/menu.tsx
+++ src/app/(tabs)/menu.tsx
@@ -1,50 +1,29 @@
-import { MaterialIcons } from '@expo/vector-icons';
 import { View, Image, Text } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type Props = {}
 
 const ProfileScreen = (props: Props) => {
   return (
-    <View className="flex-1 bg-white">
-      {/* Cabeçalho */}
-      <View className="bg-[#FF6600] p-4 flex-row items-center">
-        <Image
-          source={{ uri: 'https://via.placeholder.com/150' }} // Substituir pelo avatar correto
-          className="w-16 h-16 rounded-full"
-        />
-        <View className="ml-4">
-          <Text className="text-white text-lg">Vinicius Moreira</Text>
-          <Text className="text-white">Minha Conta</Text>
+    <SafeAreaView className='flex-1'>
+      <View className="flex-1 bg-white">
+        {/* Cabeçalho */}
+        <View className="bg-[#FF6600] p-4 flex-row items-center">
+          <Image
+            source={{ uri: 'https://via.placeholder.com/150' }}
+            className="w-16 h-16 rounded-full"
+          />
+          <View className="ml-4">
+            <Text className="text-white text-lg">Vinicius Moreira</Text>
+            <Text className="text-white">Minha Conta</Text>
+          </View>
         </View>
       </View>
-
-      {/* Menu Items */}
-      <ScrollView className="flex-1 bg-white p-4">
-        <MenuItem icon="notifications" title="Notificações" />
-        <MenuItem icon="list-alt" title="Meus Anúncios" />
-        <MenuItem icon="shopping-bag" title="Minhas Vendas" />
-        <MenuItem icon="credit-card" title="Carteira OLX Pay" />
-        <MenuItem icon="work" title="Plano Profissional" />
-        <MenuItem icon="share" title="Meu Perfil Público" />
-        <MenuItem icon="shopping-cart" title="Minhas Compras" />
-        <MenuItem icon="favorite" title="Favoritos" />
-      </ScrollView>
-    </View>
+    </SafeAreaView>
   )
 }
 
-function MenuItem({ icon, title }) {
-    return (
-      <View className="flex-row items-center justify-between py-4">
-        <View className="flex-row items-center">
-          <MaterialIcons name={icon} size={24} color="black" />
-          <Text className="ml-4 text-base text-gray-800">{title}</Text>
-        </View>
-        <MaterialIcons name="chevron-right" size={24} color="black" />
-      </View>
-    );
-  }
+
 
 export default ProfileScreen
 
